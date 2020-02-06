c3dc1e5719fab53c15b91a9e52f84c061b7b51a4
Ricky Do <ricky.do@dps.texas.gov>
2020-02-06 08:19:08 -0600

    completed lesson 3

---
+++ java21days/src/java21days/RefTester.java
@@ -0,0 +1,17 @@
+package java21days;
+
+import java.awt.Point;
+
+class RefTester {
+  public static void main(String[] arguments) {
+    Point pt1, pt2;
+    pt1 = new Point(100, 190);
+    pt2 = pt1;
+
+    pt1.x = 200;
+    pt1.y = 290;
+
+    System.out.println("Point1: " + pt1.x + ", " + pt1.y);
+    System.out.println("Point2: " + pt2.x + ", " + pt2.y);
+  }
+}
