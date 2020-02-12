20a9a74864d292e92ea79b71e12e4be52e7fc623
Ricky Do <ricky.do@dps.texas.gov>
2020-02-12 08:36:25 -0600

    halfway through lesson 5, classes

---
+++ java21days/src/java21days/Averager.java
@@ -0,0 +1,15 @@
+package java21days;
+
+class Averager {
+  public static void main(String[] args) {
+    int sum = 0;
+
+    if (args.length > 0) {
+      for (int i = 0; i < args.length; i ++) {
+        sum += Integer.parseInt(args[i]);
+      }
+      System.out.println("Sum is: " + sum);
+      System.out.println("Average is: " + (float) sum / args.length);
+    }
+  }
+}
