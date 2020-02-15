ff91833c8fd3908762d067eda130c67f82f5b2bc
Ricky Do <ricky.do@dps.texas.gov>
2020-02-15 08:36:59 -0600

    completed lesson 5

---
+++ java21days/src/java21days/Printer.java
@@ -0,0 +1,25 @@
+package java21days;
+
+class Printer {
+  int x = 0;
+  int y = 1;
+
+  void printMe() {
+    System.out.println("x is " + x + ", y is " + y);
+    System.out.println("I am an instance of a class " + this.getClass().getName());
+  }
+}
+
+class SubPrinter extends Printer {
+  int z = 3;
+
+  public static void main(String[] args) {
+    SubPrinter obj = new SubPrinter();
+    obj.printMe();
+  }
+
+  void printMe() {
+    System.out.println("x is " + x + ", y is" + y + ", z is " + z);
+    System.out.println("I am instance of the class " + this.getClass().getName());
+  }
+}
