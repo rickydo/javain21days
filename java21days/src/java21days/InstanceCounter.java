cec44b1129f38c8d4b5fd65a64cd6fcdc75b009c
Ricky Do <ricky.do@dps.texas.gov>
2020-02-15 10:33:08 -0600

    completed lesson 6 classes and interfaces

---
+++ java21days/src/java21days/InstanceCounter.java
@@ -0,0 +1,26 @@
+package java21days;
+
+public class InstanceCounter {
+  private static int numInstances = 0;
+
+  protected static int getCount() {
+    return numInstances;
+  }
+
+  private static void addInstance() {
+    numInstances++;
+  }
+
+  InstanceCounter() {
+    InstanceCounter.addInstance();
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Starting with " + InstanceCounter.getCount() + " objects");
+    for (int i=0; i < 500; i++) {
+      new InstanceCounter();
+    }
+
+    System.out.println("Created " + InstanceCounter.getCount() + " objects");
+  }
+}
