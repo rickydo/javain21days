dcb7ff9c8d64c35bb322920419b4ac867c6605bf
Ricky Do <ricky.do@dps.texas.gov>
2020-02-24 08:07:26 -0600

    lesson 9 in progress, working on frames

---
+++ java21days/src/java21days/SimpleFrame.java
@@ -0,0 +1,25 @@
+package java21days;
+
+import javax.swing.*;
+
+public class SimpleFrame extends JFrame {
+  public SimpleFrame() {
+    super("Frame Title");
+    setSize(300, 100);
+    setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+    setVisible(true);
+  }
+
+  private static void setLookAndFeel() {
+    try {
+      UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+    } catch (Exception exc) {
+      // ignore error
+    }
+  }
+
+  public static void main(String[] args) {
+    setLookAndFeel();
+    SimpleFrame sf = new SimpleFrame();
+  }
+}
